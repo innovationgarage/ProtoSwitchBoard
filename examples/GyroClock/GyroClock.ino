264369cade75f72f2242a31db9c0107e3469d01e
Erwin Ried <erwinried@gmail.com>
2017-09-26 12:29:17 +0200

    Basic functionality

--- examples/GyroClock/GyroClock.ino
+++ examples/GyroClock/GyroClock.ino
@@ -1,5 +1,14 @@
+// **********************************************************************
+// ***************************** GIRO ***********************************
+// **********************************************************************
 
-// CONNECTIONS:
+const int MPU_addr = 0x69; // I2C address of the MPU-6050
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
+// **********************************************************************
+// ***************************** RTC ************************************
+// **********************************************************************
+
 // DS1307 SDA --> SDA
 // DS1307 SCL --> SCL
 // DS1307 VCC --> 5v
@@ -19,38 +28,152 @@
 RtcDS1307<TwoWire> Rtc(Wire);
 /* for normal hardware wire use above */
 
+// **********************************************************************
+// ***************************** SEGMENT SCREEN *************************
+// **********************************************************************
+
+// include the SevenSegmentTM1637 library
+#include "SevenSegmentTM1637.h"
+#include "SevenSegmentExtended.h"
+
+/* initialize global TM1637 Display object
+   The constructor takes two arguments, the number of the clock pin and the digital output pin:
+  SevenSegmentTM1637(byte pinCLK, byte pinDIO);
+*/
+const byte PIN_CLK = 4;   // define CLK pin (any digital pin)
+const byte PIN_DIO = 5;   // define DIO pin (any digital pin)
+SevenSegmentExtended      display(PIN_CLK, PIN_DIO);
+
 void setup ()
 {
   Serial.begin(9600);
   Rtc.Begin();
+  display.begin();            // initializes the display
+  display.setBacklight(100);  // set the brightness to 100 %
+
+  Serial.print("compiled: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+
+  //--------RTC SETUP ------------
+  // if you are using ESP-01 then uncomment the line below to reset the pins to
+  // the available pins for SDA, SCL
+  // Wire.begin(0, 2); // due to limited pins, use pin 0 and 2 for SDA, SCL
+
+  Rtc.Begin();
+
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Serial.println();
+
+  if (!Rtc.IsDateTimeValid())
+  {
+    // Common Cuases:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+
+    Serial.println("RTC lost confidence in the DateTime!");
+
+    // following line sets the RTC to the date & time this sketch was compiled
+    // it will also reset the valid flag internally unless the Rtc device is
+    // having an issue
+
+    Rtc.SetDateTime(compiled);
+  }
 
   if (!Rtc.GetIsRunning())
   {
     Serial.println("RTC was not actively running, starting now");
     Rtc.SetIsRunning(true);
   }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  if (now < compiled)
+  {
+    Serial.println("RTC is older than compile time!  (Updating DateTime)");
+    Rtc.SetDateTime(compiled);
+  }
+  else if (now > compiled)
+  {
+    Serial.println("RTC is newer than compile time. (this is expected)");
+  }
+  else if (now == compiled)
+  {
+    Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+  }
+
+  // never assume the Rtc was last configured by you, so
+  // just clear them to your needed state
   Rtc.SetSquareWavePin(DS1307SquareWaveOut_Low);
+
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
 }
 
-unsigned long nextSerialReport = 0;
+unsigned long waitfor = 0, waitfor2 = 0;
+
+void handleGyro()
+{
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  /*Serial.print("AcX = "); Serial.print(AcX);
+    Serial.print(" | AcY = "); Serial.print(AcY);
+    Serial.print(" | AcZ = "); Serial.print(AcZ);
+    Serial.print(" | Tmp = "); Serial.print(Tmp / 340.00 + 36.53); //equation for temperature in degrees C from datasheet
+    Serial.print(" | GyX = "); Serial.print(GyX);
+    Serial.print(" | GyY = "); Serial.print(GyY);
+    Serial.print(" | GyZ = "); Serial.println(GyZ);
+    delay(333);*/
+
+  if (AcX > 15000 || AcX < -15000)
+  {
+    Serial.println("changing time!");
+    RtcDateTime now = Rtc.GetDateTime();
+    now += 60 * ( AcX < -15000 ? -1 : 1);
+    Rtc.SetDateTime(now);
+    waitfor2 = millis() + 100;
+    waitfor = 0;
+  }
+
+}
+
 void loop ()
 {
-  if (!Rtc.IsDateTimeValid())
-  {
-    Serial.println("RTC lost confidence in the DateTime!");
-  }
-  else
-  {
-    RtcDateTime now = Rtc.GetDateTime();
+  if (waitfor == 0)
+    waitfor = millis();
 
-    if (millis() > nextSerialReport)
+  if (waitfor2 <= millis())
+    handleGyro();
+
+  if (waitfor <= millis())
+  {
+    if (!Rtc.IsDateTimeValid())
     {
+      Serial.println("RTC lost confidence in the DateTime!");
+    }
+    else
+    {
+      RtcDateTime now = Rtc.GetDateTime();
+
       printDateTime(now);
       Serial.println();
-      nextSerialReport = millis() + 1000;
+
+      display.printTime(now.Hour(), now.Minute(), true);
     }
 
-    delay(100); // 1 seconds
+    waitfor += 1000;
   }
 }
 
